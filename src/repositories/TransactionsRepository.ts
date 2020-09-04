662087f3e6e243a412748270e1b951ad01d9a94a
RogerioPrado <eerogerioprado@gmail.com>
2020-09-04 14:25:38 -0300

    :sparkles: challenge finalized

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,8 +10,40 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
+  private async getTotalByType(type: string): Promise<number> {
+    const transactions = await this.find({ relations: ['category'] });
+    const totalTransactionsByType = transactions
+      .filter(transaction => transaction.type === type)
+      .reduce(
+        (previousValue, currentValue) => {
+          const value = {
+            value: Number(previousValue.value) + Number(currentValue.value),
+            type: previousValue.type,
+            id: '0',
+            title: '',
+          };
+
+          return value;
+        },
+        {
+          value: 0,
+          type,
+          title: '',
+          id: '0',
+        },
+      );
+
+    return totalTransactionsByType.value;
+  }
+
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const totalOfIncome = await this.getTotalByType('income');
+    const totalOfOutcome = await this.getTotalByType('outcome');
+    return {
+      income: totalOfIncome,
+      outcome: totalOfOutcome,
+      total: totalOfIncome - totalOfOutcome,
+    };
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,83 @@
+import parse from 'csv-parse';
+import fs from 'fs';
+import { getRepository, In, getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CSVTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filename: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const csvTransactions: CSVTransaction[] = [];
+    const categories: string[] = [];
+
+    const transactionReadStream = fs.createReadStream(filename);
+    const csvParser = parse({
+      from_line: 2,
+    });
+
+    transactionReadStream.pipe(csvParser);
+    csvParser.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !value || !type || !category) return;
+
+      categories.push(category);
+
+      csvTransactions.push({
+        title,
+        value: Number(value),
+        type,
+        category,
+      });
+    });
+    await new Promise(resolve => csvParser.on('end', resolve));
+
+    const categoryRepository = getRepository(Category);
+    const existentCategories = await categoryRepository.find({
+      where: { title: In(categories) },
+    });
+
+    const existentCategoriesTitle = existentCategories.map(
+      existentCategory => existentCategory.title,
+    );
+
+    const addCategoriesTitle = categories
+      .filter(category => !existentCategoriesTitle.includes(category))
+      .filter((category, index, self) => self.indexOf(category) === index);
+
+    const newCategories = addCategoriesTitle.map(title =>
+      categoryRepository.create({ title }),
+    );
+
+    if (newCategories.length > 0) {
+      await categoryRepository.insert(newCategories);
+    }
+
+    const finalCategories = [...newCategories, ...existentCategories];
+    const transactions: Transaction[] = csvTransactions.map(transaction => {
+      return transactionRepository.create({
+        title: transaction.title,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+        type: transaction.type,
+      });
+    });
+
+    transactionRepository.save(transactions);
+    await fs.promises.unlink(filename);
+    return transactions;
   }
 }
 
